36148ec2ed2a61a34a1be180d182e999eea255eb
Slayer Bryan <bryan.antonio.alvarado.villalobos@gmail.com>
2019-12-16 12:05:37 -0500

    Commit Inicial

---
+++ src/main/java/com/slayer/api/ubigeo/SpringUbigeoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.slayer.api.ubigeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUbigeoAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUbigeoAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slayer/api/ubigeo/SpringUbigeoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slayer.api.ubigeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUbigeoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
